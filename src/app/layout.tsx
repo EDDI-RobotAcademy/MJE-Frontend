059ab19ba250e3d20a640ff4ee3d16c63d73a62f
imjina067-hue <imjina067@gmail.com>
2026-04-30 14:29:48 +0900

    feat : [임진아] 헤더 로고 UI 노출  [MJE-FE-1]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/home/ui/layout/Header";
 
 export const metadata: Metadata = {
   title: "MJE Frontend",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
